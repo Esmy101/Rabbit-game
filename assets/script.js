fa748d246e45b177fb58e7aec9048307f8572de8
Esmeralda Romero <esmeralda.r.a@hotmail.com>
2022-08-27 13:54:18 -0400

    commets

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,17 @@
 const rabbit = document.getElementById("rabbit");
 const carrot = document.getElementById("carrot");
+
 function jump() {
+  //making rabbit jump
   if (dispatchEvent.classList != "jump") {
+    //first it checks if the rabbit is mid-jump. If not, it makes it jump.
     rabbit.classList.add("jump");
     setTimeout(function () {
-      raddit.classList.remove("jump");
+      rabbit.classList.remove("jump");
     }, 300);
   }
 }
+
 let checkAlive = setInterval(function () {
   let rabbitTop = parseInt(
     window.getComputedStyle(rabbit).getPropertyValue("top")
@@ -15,10 +19,11 @@ let checkAlive = setInterval(function () {
   let carrotLeft = parseInt(
     window.getComputedStyle(carrot).getPropertyValue("left")
   );
+  //check for collision
   if (carrotLeft > 0 && carrotLeft < 70 && rabbitTop >= 143) {
     rabbit.style.animationPlayState = "paused";
     carrot.style.animationPlayState = "paused";
-    alert("Game Over :( ");
+    alert("Game Over");
     window.location.reload();
   }
 }, 10);
